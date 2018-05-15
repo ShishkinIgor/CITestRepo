02cb1f2b4ac9058190ff8e510fd93a085cb5fc40
IgorShyshkin <igor.shyshkin@leiainc.com>
2018-05-15 19:26:55 +0300

    Add simple editor test

---
+++ CITestProject/Assets/Editor/SimpleEditModeTest.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class SimpleEditModeTest {
+
+	[Test]
+	public void SuccessfulPass() {
+
+        Assert.Pass("Test pass.");
+	}
+
+    [Test]
+    public void FailedPass()
+    {
+        Assert.Fail("Test failed.");
+    }
+}
