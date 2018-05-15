9fd1d79a60150d2f2d391d84421554266cc35041
IgorShyshkin <igor.shyshkin@leiainc.com>
2018-05-15 19:52:38 +0300

    Add new Scene

---
+++ CITestProject/Assets/Scrits/Spin.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour {
+
+    public Vector3 EulerAngle = Vector3.zero;
+
+	void Update () {
+        var rotation = transform.rotation;
+        rotation.eulerAngles += EulerAngle * Time.deltaTime;
+        transform.rotation = rotation;
+    }
+}
