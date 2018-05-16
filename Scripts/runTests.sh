f2eaa3e281d047f7ffbbded97036b87b1761ee91
IgorShyshkin <igor.shyshkin@leiainc.com>
2018-05-16 19:57:00 +0300

    Make Travis execute tests

---
+++ Scripts/runTests.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+# Example build script for Unity3D project. See the entire example: https://github.com/JonathanPorta/ci-build
+
+# Change this the name of your project. This will be the name of the final executables as well.
+project="CITestProject"
+
+#echo "Attempting to build $project for Windows"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+  -batchmode \
+  -nographics \
+  -silent-crashes \
+  -projectPath $(pwd) \
+  -runEditorTests
+  -quit
+
+echo "UnitTest COMPLETED! Exited with $?"
