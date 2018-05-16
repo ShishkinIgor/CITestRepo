f5e3a7d3a9d12020b190055edc00da5aab664cf5
IgorShyshkin <igor.shyshkin@leiainc.com>
2018-05-16 18:48:50 +0300

    Change build target

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -5,15 +5,15 @@
 # Change this the name of your project. This will be the name of the final executables as well.
 project="CITestProject"
 
-echo "Attempting to build $project for Windows"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-  -batchmode \
-  -nographics \
-  -silent-crashes \
-  -logFile $(pwd)/unity.log \
-  -projectPath $(pwd) \
-  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
-  -quit
+#echo "Attempting to build $project for Windows"
+#/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+#  -batchmode \
+#  -nographics \
+#  -silent-crashes \
+#  -logFile $(pwd)/unity.log \
+#  -projectPath $(pwd) \
+#  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
+#  -quit
 
 echo "Attempting to build $project for OS X"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
