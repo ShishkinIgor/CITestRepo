dceb4de9b5a835c175862472f2d6732678cc9434
IgorShyshkin <igor.shyshkin@leiainc.com>
2018-05-16 21:43:31 +0300

    Fail commit on failed tests

--- Scripts/runTests.sh
+++ Scripts/runTests.sh
@@ -1,11 +1,6 @@
 #! /bin/sh
-
-# Example build script for Unity3D project. See the entire example: https://github.com/JonathanPorta/ci-build
-
-# Change this the name of your project. This will be the name of the final executables as well.
 project="CITestProject"
 
-#echo "Attempting to build $project for Windows"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
   -nographics \
@@ -13,4 +8,12 @@ project="CITestProject"
   -projectPath $(pwd) \
   -runEditorTests
 
-echo "UnitTest COMPLETED! Exited with $?"
+if [ $? = 0 ] ; then
+  echo "UnitTests COMPLETED! Exited with $?"
+  exit 0
+else
+  echo "UnitTests FAILED! Exited with $?"
+  echo 'Test logs'
+  cat $(pwd)/integrationTests.log
+  exit 1
+fi
