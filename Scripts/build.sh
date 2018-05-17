74dc67ec40cb7717e44d66b3cff867117bb785fa
IgorShyshkin <igor.shyshkin@leiainc.com>
2018-05-17 21:09:40 +0300

    c

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -3,11 +3,11 @@
 # Example build script for Unity3D project. See the entire example: https://github.com/JonathanPorta/ci-build
 
 # Change this the name of your project. This will be the name of the final executables as well.
-project="CITestProject"
+project="/CITestProject"
 buildSettings="/ProjectSettings/EditorBuildSettings.asset"
 
 echo "Build setting:"
-cat $(pwd)$buildSettings
+cat $(pwd)$project$buildSettings
 
 echo "Attempting to build $project for Windows"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
@@ -16,7 +16,7 @@ echo "Attempting to build $project for Windows"
   -silent-crashes \
   -logFile $(pwd)/unity.log \
   -projectPath $(pwd) \
-  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
+  -buildOSXUniversalPlayer "$(pwd)/Build/windows$project.exe" \
   -quit
 
 echo 'Logs from build'
