6b5acba20e39aea7641d70ca5dbd936e4deff4aa
IgorShyshkin <igor.shyshkin@leiainc.com>
2018-05-21 18:02:04 +0300

    BuildScript

---
+++ CITestProject/Assets/Editor/BuildScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class BuildScript : MonoBehaviour {
+
+    [MenuItem("Build/Build OS X")]
+    public static void BuildOSX()
+    {
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = new[] { "Assets/Scenes/TestScene1.unity" };
+        buildPlayerOptions.locationPathName = "Builds/OSXBuild";
+        buildPlayerOptions.target = BuildTarget.StandaloneOSXUniversal;
+        buildPlayerOptions.options = BuildOptions.None;
+        BuildPipeline.BuildPlayer(buildPlayerOptions);
+    }
+
+    [MenuItem("Build/Build Win")]
+    public static void BuildWin()
+    {
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = new[] { "Assets/Scenes/TestScene1.unity" };
+        buildPlayerOptions.locationPathName = "Builds/WinBuild";
+        buildPlayerOptions.target = BuildTarget.StandaloneWindows;
+        buildPlayerOptions.options = BuildOptions.None;
+        BuildPipeline.BuildPlayer(buildPlayerOptions);
+    }
+}
