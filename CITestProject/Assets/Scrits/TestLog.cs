bb22faa2689baf8c471da34cbeebb8f5cf3683de
IgorShyshkin <igor.shyshkin@leiainc.com>
2018-08-10 17:40:41 +0300

    Add logs

---
+++ CITestProject/Assets/Scrits/TestLog.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestLog : MonoBehaviour {
+
+    float timeToNextMsg = 0;
+    float MsgDelay = 1f;
+
+	// Update is called once per frame
+	void Update () {
+        timeToNextMsg -= Time.deltaTime;
+        if (timeToNextMsg <= 0)
+        {
+            timeToNextMsg += MsgDelay;
+            Debug.Log("Log Message");
+            Debug.LogWarning("Warning Message");
+            Debug.LogError("Error Message");
+        }
+	}
+}
